48c4a393dae159e794116f872ba899b260d41e3c
abhitech2002 <oabhishek31@gmail.com>
2024-09-23 19:56:19 +0530

    variables and datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 266631
+let accountEmail = "abhishek@gmail.com"
+var accountPassword = "23332"
+accountCity = "jaipur"
+let accountState
+
+accountEmail = 'abhi@gmail.com'
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+// Don't use the var because of the issue in block scope and function scoped
---
+++ basics/02_datatypes.js
@@ -0,0 +1,32 @@
+// "use strict" // treat all js code as newer version
+
+// alert(3 + 3)  we re using node js not browser
+
+console.log(3 + 3);
+
+console.log("Abhishek")
+
+let name = "Abhishk"
+let age = 29
+let isLoggedIn = false
+
+// Datatype
+
+// console.log(String);
+
+// primitive datatypes
+// Number
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol => unique
+
+// object
+
+// console.log(typeof null); // output: object
+
+// console.log(typeof undefined); // output: undefined
+
+
