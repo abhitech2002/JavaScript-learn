f653bced7f605943f9f4519b7bbbb2fe0d308668
abhitech2002 <oabhishek31@gmail.com>
2024-10-23 10:28:05 +0530

    Basic Structure with Django

---
+++ chaiaurdjango/chaiaurdjango/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'index.htm')
+    # return HttpResponse("Hello, world. You're at the chaiaurdjango index.")
+
+def about(request):
+    return HttpResponse("Hello, world. You're at the chaiaurdjango about.")
+
+def contact(request):
+    return HttpResponse("Hello, world. You're at the chaiaurdjango contact.")
